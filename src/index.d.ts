649f3edf1ff0a95686dff83b243ff4c58065f5ea
Matanya <mloewenthal@groq.com>
2025-02-02 15:27:09 -0500

    fix: actually generate typescript

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * as SoferaiApi from "./api";
-export { SoferaiApiClient } from "./Client";
-export { SoferaiApiEnvironment } from "./environments";
-export { SoferaiApiError, SoferaiApiTimeoutError } from "./errors";
